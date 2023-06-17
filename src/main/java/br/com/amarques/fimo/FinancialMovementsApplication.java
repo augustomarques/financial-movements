fa32dd64a3915bfd2fb194d3d4d8a73fba535eed
Augusto Marques <augusto@Augustos-MacBook-Pro.local>
2023-06-16 22:55:29 -0300

    Starting project

---
+++ src/main/java/br/com/amarques/fimo/FinancialMovementsApplication.java
@@ -0,0 +1,13 @@
+package br.com.amarques.fimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialMovementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialMovementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/amarques/fimo/FinancialMovementsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.amarques.fimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialMovementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
